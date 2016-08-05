12a1a5a2160a30c0da5596b8c7179b68778f3feb
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-05 11:16:53 -0500

    output to DOM sucessful

--- carLot.js
+++ carLot.js
@@ -3,69 +3,26 @@
 
 console.log ('The carLot.js page is good to go!');
 
-var CarLot = (function () {
+  var CarLot = (function () {
   var inventory = [];
 
   return {
     getInventory: function () {
 
     },
+
     loadInventory: function (callback) {
       var inventoryLoader = new XMLHttpRequest();
-      xhr.open('GET', 'inventory.json')
-      inventoryLoader.addEventListener("load", function (event) {
-      newInventory= JSON.parse(event.target.responseText).cars
+      inventoryLoader.open('GET', 'inventory.json')
+      inventoryLoader.addEventListener("load", function () {
+        inventory = JSON.parse(inventoryLoader.responseText).cars
+        callback(inventory)
       });
-
-      xhr.send()
+      inventoryLoader.send()
     }
+
+
   };
 
-    populatePage: function () {
-      var container= document.querySelector("#bigContainer")
-      newInventory.forEach(function(cars){
-      container.innerHTML += `
+  })();
 
-        <div class="row">
-        <div class="col-sm-4">
-
-
-    </div>`
-
-
-
-      })
-
-
-
-    }
-
-})();
-
-
-// var moreSongs = (function () {
-//   var loadSongs = [];
-
-//   return {
-//     getJSONSongs: function (callback) {
-//       var xhr = new XMLHttpRequest()
-//       xhr.open('GET', 'songs.json')
-//       xhr.addEventListener('load', function (event) {
-//         songsJSON = JSON.parse(event.target.responseText).songs
-//         callback(songsJSON)
-//       })
-//       xhr.send()
-//     },
-
-    // populatePage: function () {
-    //   var container = document.querySelector("#right_side")
-
-////////////// Outputs JSON text string to DOM.  Adds delete button on each song listing.////////////////
-      // songsJSON.forEach(function(songs) {
-        container.innerHTML += `
-          <p>${songs.Song} by
-          ${songs.Artist} on the album
-          ${songs.Album}  <button type="button" class="deleteButton"  name = "delete" >Delete</button></p>`
-      })
-    },
-  }
--- quiz.js
+++ quiz.js
@@ -1,32 +1,43 @@
 // here is the beginning of my awesome code!!!
 console.log ('Quiz.js page is good to go!')
 
-function populatePage (inventory) {
+function populatePage (cars) {
   // Loop over the inventory and populate the page
 
+  var container = document.querySelector("#bigContainer")
+  cars.forEach(function (car) {
+  container.innerHTML +=
 
-
+        `
+    		 <div class="col-md-4">
+   			 <p><b>Make:</b> ${car.make}</p>
+         <p><b>Model:</b> ${car.model}</p>
+         <p><b>Year:</b> ${car.year}</p>
+         <p><b>Price:</b> ${car.price}</p>
+         <p><b>Color:</b> ${car.color}</p>
+         <p><b>Purchased:</b> ${car.purchased}</p>
+         <p><b>Description:</b> ${car.description}</p>
+  			</div>
+  			`
+  })
 
 // The second IIFE should augment the original one with a function that creates all of the `eventHandlers`
 // that you need for the application. Name the function `activateEvents`.
   // Now that the DOM is loaded, establish all the event listeners needed
   function activateEvents(){
-
+  	var userInput = document.getElementById("userInput");
+		var userSubmit = document.getElementById("userSubmit");
 
 
 
   }
-  CarLot.activateEvents();
 
-
-
-
-
-
+  // CarLot.activateEvents();
 }
+//////////end of IIFE (populate page)////////////////////////////////
 
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
-CarLot.loadInventory();
+CarLot.loadInventory(populatePage);
 
 
